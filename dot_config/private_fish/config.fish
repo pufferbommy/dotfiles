b9c92acae3bc98f5031b79bf7f65d129c42caf85
pufferbommy <pufferbommy@gmail.com>
2023-06-20 19:43:23 +0700

    feat: add asdf and uninstall some plugins

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,7 @@
-set nvm_default_version v16.16.0
-
 set -gx TERM xterm-256color
 set -gx EDITOR nvim
 
-source ~/.config/fish/aliases.fish
 zoxide init fish | source
 
-function fish_greeting
-    echo "Current node version: $nvm_current_version"
-end
+source ~/.config/fish/aliases.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
