e9834499e5514279a579d686b5fd35857db07d68
pufferbommy <pufferbommy@gmail.com>
2024-05-09 09:50:21 +0700

    update

--- dot_config/private_fish/aliases.fish
+++
@@ -1,4 +0,0 @@
-alias cl "clear"
-alias l "ls -l"
-alias s "nr start"
-alias d "nr dev"
--- dot_config/private_fish/config.fish
+++
@@ -1,9 +0,0 @@
-fish_add_path /Applications/Docker.app/Contents/Resources/bin/
-
-set -gx TERM xterm-256color
-set -gx EDITOR nvim
-
-zoxide init fish | source
-
-source ~/.config/fish/aliases.fish
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
